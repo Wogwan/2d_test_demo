5c8890025ce9dbae29050580c434b3323ccfe7cb
Wogwan <2284079411@qq.com>
2021-10-14 15:58:54 +0800

    Update for Less rsme 16:00

--- test_65_InvertPendulum.m
+++ test_65_InvertPendulum.m
@@ -6,11 +6,11 @@ format long
 %%
 syms x1 x2
 dttr = 0.01;                                               % Recording step size for taining data (default = 0.3)
-Ttr = 10;                                                 % Simulation time for training per starting point (default = 3)
+Ttr = 20;                                                 % Simulation time for training per starting point (default = 3)
 ntr = floor(Ttr/dttr);
-noise = 0.00001;                                             % Obervation noise
+noise = 1e-3;                                             % Obervation noise
 sn = noise*[1 1]';                                        % Observation noise (default = 1e-1)
-it = 600;
+it = 1000;
 %     it = real(size(Xtr_1)/300*200);
 poly_deg = 4;
 dxdt2 = 0;
@@ -22,7 +22,7 @@ miu = 0.05;
 
 %% Chebyshev interpolants value
 tic
-sz = 6;
+sz = 8;
 deg = 4;
 f2_p = -miu/(m*l^2)*x2;
 f2_np = g/l*sin(x1);
@@ -49,7 +49,7 @@ toc
 % Set parameters
 dXtr_0 = [];                                              % Collecting training model for learning the difference in real and approximated dynamic systems
 Xtr_0 = [];                                               % Collecting the Xtr_1 = [x1 x2] data by ode45 with setting
-x0tr = [-0.5 -0.4; 0.2 -0.4];
+x0tr = [-0.6 -0.6; 0.4 0.4];
 % x0test = [; ];
 
 E = 2;                                                    % Dimensions of the state space
@@ -120,16 +120,21 @@ end
 
 %%
 [mean1,hyp1,delta,rmse] = gpr_xdot2(x,y,xtest,ytest,it,noise,poly_deg);            % GP learning for the xdot2 dynamic systems
+%%
 pvar x1 x2
-X1 = p2s(monomials(x1,(0:poly_deg)));
-X2 = p2s(monomials(x2,(0:poly_deg)));
-for i = 2:length(X1)
+X1 = p2s(monomials(x1,(1:poly_deg)));
+X2 = p2s(monomials(x2,(1:poly_deg)));
+for i = 1:length(X1)
     X = [X; X1(i); X2(i)];
 end
 for i = 1:length(X)
     dxdt2 = vpa(dxdt2 + X(i)*mean1(i));
 end
+%%
+% deg = 3;
+% dxdt2 = (mean1(1)+mean(2)*x1+mean(3)*x2)^deg;
 
+%%
 g_learn = f2_appro + dxdt2
 figure(2); clf; syms x1 x2;
 y1 = double(subs(g_learn,{x1,x2},{xtest(:,1),xtest(:,2)}));
