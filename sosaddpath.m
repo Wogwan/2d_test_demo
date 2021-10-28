23599522ac28b3ba3d0e7be8b6184257638a3d49
Wogwan <865979137@qq.com>
2021-10-28 11:47:16 +0800

    Set up usual 2D files

--- sosaddpath.m
+++ sosaddpath.m
@@ -6,78 +6,69 @@
 cm = computer;
 
 if cm(1) == 'M' ||  cm(1)=='G'
-%     % Add chebfun-master
-%     addpath([pwd '/toolbox/chebfun-master']);
+    % Add chebfun-master
+    addpath([pwd '/toolbox/chebfun-master']);
     
     % Add gpml-matlab-master
-    addpath([pwd '/toolbox/gpml']); 
-    % Please run up the '/gpml-matlab-master/startup.m' if you first
-    % running the code
+    addpath([pwd '/toolbox/gpml']);
+    run([pwd '/toolbox/gpml/startup.m'])
     
     % Add multipoly
     addpath([pwd '/toolbox/multipoly']);
     
     % Add nlanal
     addpath([pwd '/toolbox/nlanal']);
-
+    
     % Add my version of SOSTools
     addpath([pwd '/toolbox/sosopt']);
     addpath([pwd '/toolbox/sosopt/Demos']);
-
+    
     % Add polysys
     addpath([pwd '/toolbox/polysystems_1_0_3'])
     addpath([pwd '/toolbox/polysystems_1_0_3/demo'])
-
-%     % Add SeDuMi_1_3
-%     addpath([pwd '/toolbox/SeDuMi_1_3'])
     
     % Add utils
     addpath([pwd '/utils'])
     
     % Add updates
     addpath([pwd '/updates'])
-        
-    % Add test_function
-    addpath([pwd '/test_function'])
-    addpath([pwd '/test_function/paper_example'])
-
+    
+    % Add demo_2D
+    addpath([pwd '/demo_2D'])
+    addpath([pwd '/demo_2D/paper_example'])
+    
 elseif cm(1) == 'P'
-
-%     % Add chebfun-master
-%     addpath([pwd '/toolbox/chebfun-master']);
+    
+    % Add chebfun-master
+    addpath([pwd '/toolbox/chebfun-master']);
     
     % Add gpml-matlab-master
     addpath([pwd '/toolbox/gpml']);
-    % Please run up the '/gpml-matlab-master/startup.m' if you first
-    % running the code
+    run([pwd '/toolbox/gpml/startup.m'])
     
     % Add multipoly
     addpath([pwd '/toolbox/multipoly']);
     
     % Add nlanal
     addpath([pwd '/toolbox/nlanal']);
-
+    
     % Add my version of SOSTools
     addpath([pwd '/toolbox/sosopt']);
     addpath([pwd '/toolbox/sosopt/Demos']);
-
+    
     % Add polysys
     addpath([pwd '/toolbox/polysystems_1_0_3'])
     addpath([pwd '/toolbox/polysystems_1_0_3/demo'])
-
-    % Add SeDuMi_1_3
-%     addpath([pwd '/toolbox/SeDuMi_1_3'])
     
     % Add utils
     addpath([pwd '/utils'])
-            
-%     Add updates
-    addpath([pwd '/updates'])
-            
-    % Add test_function
-    addpath([pwd '/test_function'])
-    addpath([pwd '/test_function/paper_example'])
-
-end
     
+    % Add updates
+    addpath([pwd '/updates'])
+    
+    % Add demo_2D
+    addpath([pwd '/demo_2D'])
+    addpath([pwd '/demo_2D/paper_example'])
+end
+
 
